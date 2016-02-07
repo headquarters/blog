0da06c5ec14762fa3ed189123debdc2244591bd4
Michael Head <michaelehead@gmail.com>
2016-02-07 13:43:54 -0500

    UI improvements to portfolio

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,22 +1,22 @@
 (function($){
-	$(".slideshow a").each(function(){
+	$(".image-link").each(function(){
 	    var $this = $(this);
-	    
+
 	    var href = $this.attr("href");
 	    var text = $this.text();
 	    $this.parent().attr("data-featherlight", href);
-	    
+
 	    var img = document.createElement("img");
 	    img.src = href;
 	    img.alt = text;
-	    
-	    $this.replaceWith(img);   
-	});
 
-
-	$(".slideshow").slick({
-	    infinite: false
+	    $this.replaceWith(img);
 	});
+    //
+    //
+	// $(".slideshow").slick({
+	//     infinite: false
+	// });
 
 	$(".post img").each(function(){
 		$(this).attr("title", "Click to expand");
